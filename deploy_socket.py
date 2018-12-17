2bed0c2bfa7cac9fb63e24be1721ed944b195a9a
Kasper <kpa@clerk.io>
2018-12-17 13:09:01 +0000

    work work

---
+++ deploy_socket.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import subprocess
+import socket
+import json
+import os
+import threading
+
+ADDR = '0.0.0.0'
+PORT = 5004
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((ADDR, PORT))
+s.listen(10)
+
+
+# Read config
+config = {}
+for c in open('/etc/deploytoy.conf', 'r').readlines():
+    key, value = c.replace('\n','').split(' ')
+    config[key] = value
+
+def pull_repo(repo_path):
+    os.chdir(repo_path)
+    subprocess.call(['git','pull'])
+    subprocess.call(['sh','deploy.sh'])
+
+print config
+while True:
+    conn, addr = s.accept()
+    try:
+        data = json.loads(conn.recv(20000))
+        if data['repository']['full_name'] == config['repo_full_name']:
+            thread = threading.Thread(target = pull_repo, args=(config['repo_root'], ) )
+            thread.run()
+    except Exception as e: # Die gracefully
+        print "failed :(", e
---
+++ setup.py
@@ -0,0 +1,31 @@
+
+# Get input for config
+print "Need more info for creating the config file"
+repo_full_name = raw_input("Full repository name (i.e 'KPassov/deploytoy'):")
+
+repo_root = raw_input("Where should i place the repo? (i.e '~/deploytoy' ")
+
+
+# Clone Repo
+
+
+# Create Deploy key
+
+
+# Create Webhook
+
+# Success!
+# Create Config File (default /etc/deploytoy.conf)
+# Create Websocket file (default ~/deploytoy.py)
+# Add websocket file to reboot
+
+# Start websocket
+
+print "Try to clone"
+
+
+
+
+
+
+
